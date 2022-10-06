46fd769378eb48353ec70c18d3c08cc394b090b0
ibrahim <aydinibrahim1692@gmail.com>
2022-10-06 13:58:50 +0300

    Viewmodel edited

--- app/src/main/java/com/example/cryptocurreny/viewmodel/CoinListViewModel.kt
+++ app/src/main/java/com/example/cryptocurreny/viewmodel/CoinListViewModel.kt
@@ -1,9 +1,42 @@
 package com.example.cryptocurreny.viewmodel
 
+import androidx.compose.runtime.mutableStateOf
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.cryptocurreny.model.CoinListItem
+import com.example.cryptocurreny.repository.CoinRepository
+import com.example.cryptocurreny.util.Resource
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
+import javax.inject.Inject
 
 @HiltViewModel
-class CoinListViewModel {
+class CoinListViewModel @Inject constructor(private val repository: CoinRepository) : ViewModel() {
+    var coinList = mutableStateOf<List<CoinListItem>>(listOf())
+    var isLoading = mutableStateOf(false)
+    var isError = mutableStateOf("")
 
-    
+    fun loadCoins() {
+        viewModelScope.launch {
+            isLoading.value = true
+            when (val result = repository.getCoinList()) {
+                is Resource.Success -> {
+                    val coinItems = result.data
+                    val coinListItems = result.data!!.mapIndexed { index, coinListItem ->
+                        CoinListItem(coinListItem.currency, coinListItem.price)
+                    }
+                    isError.value = ""
+                    isLoading.value = false
+                    coinList.value += coinListItems
+                }
+                is Resource.Error -> {
+                    isError.value = result.message!!
+                    isLoading.value = false
+                }
+                else -> {
+                    isError.value = "An unknown error occured"
+                }
+            }
+        }
+    }
 }
